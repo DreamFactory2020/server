417dfecd81833683e7471d0633f4887b8d0d6212
YunJaeEun <yunmegan44@sookmyung.ac.kr>
2020-12-18 21:31:06 +0900

    주석 추가

--- Assets/scripts/LobbyManager.cs
+++ Assets/scripts/LobbyManager.cs
@@ -18,8 +18,8 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     private void Start()
     {
-        PhotonNetwork.GameVersion = gameVersion;
-        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.GameVersion = gameVersion;    //게임버전
+        PhotonNetwork.ConnectUsingSettings();   //마스터서버 접속 시도
         SearchRBtn.interactable = false;
         ConnectText.text = "마스터 서버 접속 중...";
 
@@ -28,20 +28,20 @@ public class LobbyManager : MonoBehaviourPunCallbacks
             (word) => Name.text = Regex.Replace(word, @"[^0-9a-zA-Z가-힣]", ""));
     }
 
-    public override void OnConnectedToMaster()
+    public override void OnConnectedToMaster()//마스터서버 접속 성공시 자동 실행
     {
         SearchRBtn.interactable = true;
         ConnectText.text = "온라인";
     }
 
-    public override void OnDisconnected(DisconnectCause cause)
+    public override void OnDisconnected(DisconnectCause cause)//마스터서버 접속 실패시 자동 실행
     {
         SearchRBtn.interactable = false;
         ConnectText.text = "오프라인. 재접속 시도...";
         PhotonNetwork.ConnectUsingSettings();
     }
 
-    public void Connect() {
+    public void Connect() { //룸 접속 시도
         SearchRBtn.interactable = false;
         if (PhotonNetwork.IsConnected)
         {
@@ -55,14 +55,14 @@ public class LobbyManager : MonoBehaviourPunCallbacks
                 ConnectText.text = "룸에 접속 중...";
                 PhotonNetwork.JoinRoom(RoomName.text);//
             }
-        }
+        }   
         else {
             ConnectText.text = "오프라인 마스터 서버 재접속 중...";
             PhotonNetwork.ConnectUsingSettings();
         }
     }
 
-    public void Create() {
+    public void Create() {  //방 생성
         SearchRBtn.interactable = false;
         if (PhotonNetwork.IsConnected)
         {
@@ -79,16 +79,16 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         }
     }
 
-    public override void OnJoinRoomFailed(short returnCode, string message)
+    public override void OnJoinRoomFailed(short returnCode, string message) //룸 참가 실패시 자동 실행
     {
         ConnectText.text = "방 없음";
         SearchRBtn.interactable = true;
     }
 
-    public override void OnJoinedRoom()
+    public override void OnJoinedRoom() //룸 참가 성공 시 자동 실행
     {
         ConnectText.text = "방참가 성공";
-        PhotonNetwork.LoadLevel("Game");
+        PhotonNetwork.LoadLevel("Game");    //모든 참가자가 Game씬을 로드한다.
     }
     public void Disconnect()    //메인창으로 돌아가기
     {
