462221364e6247287751ad597582521146191f67
YunJaeEun <yunmegan44@sookmyung.ac.kr>
2020-12-19 02:01:24 +0900

    카메라 제어, 캐릭터 조이스틱 기능

---
+++ Assets/scripts/CameraSetup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using Photon.Pun;
+using UnityEngine;
+
+public class CameraSetup : MonoBehaviourPun
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (photonView.IsMine) {
+            CinemachineVirtualCamera FollowCam = FindObjectOfType<CinemachineVirtualCamera>();
+            FollowCam.Follow = transform;
+            FollowCam.LookAt = transform;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
