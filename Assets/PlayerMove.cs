292b222b76f78476eb6cbf4b9ed6be93adf68ec3
Ujaa <40076944+Ujaa@users.noreply.github.com>
2020-12-19 20:17:58 +0900

    캐릭터 움직임 수정
    캔버스에 조이스틱 스크립트 연결해둔 거 조이스틱이랑 캐릭터로 분리

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    private JoyStick joystick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    void Awake()
+    {
+        joystick = GameObject.FindObjectOfType<JoyStick>();
+    }
+
+    void FixedUpdate()
+    {
+        if (joystick.Horizontal != 0 || joystick.Vertical != 0)
+        {
+            MoveControl();
+        }
+    }
+
+    private void MoveControl()
+    {
+        Vector3 upMovement = Vector3.up * speed * Time.deltaTime * joystick.Vertical;
+        Vector3 rightMovement = Vector3.right * speed * Time.deltaTime * joystick.Horizontal;
+        transform.position += upMovement;
+        transform.position += rightMovement;
+    }
+}
--- Assets/scripts/JoyStick.cs
+++ Assets/scripts/JoyStick.cs
@@ -2,79 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-public class JoyStick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+
+public class JoyStick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
 {
-    RectTransform m_rectBack;
-    RectTransform m_rectJoystick;
+    public RectTransform handle;
+    public RectTransform outLine;
 
-    Transform m_trCube;
-    float m_fRadius;
-    float m_fSpeed = 1.0f;
-    float m_fSqr = 0f;
+    private float deadZone = 0;
+    private float handleRange = 1;
+    private Vector3 input = Vector3.zero;
+    private Canvas canvas;
 
-    Vector3 m_vecMove;
-
-    Vector2 m_vecNormal;
-
-    bool m_bTouch = false;
-
+    public float Horizontal { get { return input.x; } }
+    public float Vertical { get { return input.y; } }
 
     void Start()
     {
-        m_rectBack = transform.Find("JoyStickBG").GetComponent<RectTransform>();
-        m_rectJoystick = transform.Find("JoyStickBG/JoyStick").GetComponent<RectTransform>();
-
-        m_trCube = GameObject.Find("Player1").transform;
-
-        // JoystickBackground의 반지름입니다.
-        m_fRadius = m_rectBack.rect.width * 0.5f;
-    }
-
-    void Update()
-    {
-        if (m_bTouch)
-        {
-            m_trCube.position += m_vecMove;
-        }
-
-    }
-
-    void OnTouch(Vector2 vecTouch)
-    {
-        Vector2 vec = new Vector2(vecTouch.x - m_rectBack.position.x, vecTouch.y - m_rectBack.position.y);
-
-
-        // vec값을 m_fRadius 이상이 되지 않도록 합니다.
-        vec = Vector2.ClampMagnitude(vec, m_fRadius);
-        m_rectJoystick.localPosition = vec;
-
-        // 조이스틱 배경과 조이스틱과의 거리 비율로 이동합니다.
-        float fSqr = (m_rectBack.position - m_rectJoystick.position).sqrMagnitude / (m_fRadius * m_fRadius);
-
-        // 터치위치 정규화
-        Vector2 vecNormal = vec.normalized;
-
-        m_vecMove = new Vector3(vecNormal.x * m_fSpeed * Time.deltaTime * fSqr, vecNormal.y * m_fSpeed * Time.deltaTime * fSqr, 0f);
-        //m_vecMove = new Vector3(vecNormal.x * m_fSpeed * Time.deltaTime * fSqr, 0f, vecNormal.y * m_fSpeed * Time.deltaTime * fSqr);
-        //m_trCube.eulerAngles = new Vector3(0f, Mathf.Atan2(vecNormal.x, vecNormal.y) * Mathf.Rad2Deg, 0f);
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        OnTouch(eventData.position);
-        m_bTouch = true;
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        outLine = gameObject.GetComponent<RectTransform>();
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        OnTouch(eventData.position);
-        m_bTouch = true;
+        OnDrag(eventData);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 radius = outLine.sizeDelta / 2;
+        input = (eventData.position - outLine.anchoredPosition) / (radius * canvas.scaleFactor);
+        HandleInput(input.magnitude, input.normalized);
+        handle.anchoredPosition = input * radius * handleRange;
+    }
+
+    private void HandleInput(float magnitude, Vector2 normalised)
+    {
+        if (magnitude > deadZone)
+        {
+            if (magnitude > 1)
+            {
+                input = normalised;
+            }
+        }
+        else
+        {
+            input = Vector2.zero;
+        }
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        // 원래 위치로 되돌립니다.
-        m_rectJoystick.localPosition = Vector2.zero;
-        m_bTouch = false;
+        input = Vector2.zero;
+        handle.anchoredPosition = Vector2.zero;
+
     }
 }
