1c3bff01c35e6d280918f8294b7d2736e4917f08
YunJaeEun <yunmegan44@sookmyung.ac.kr>
2020-12-20 02:59:56 +0900

    멀티 구현
    플레이어 인스턴스 생성 가능

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public static GameManager instance
+    {
+        get
+        {
+            // 만약 싱글톤 변수에 아직 오브젝트가 할당되지 않았다면
+            if (m_instance == null)
+            {
+                // 씬에서 GameManager 오브젝트를 찾아 할당
+                m_instance = FindObjectOfType<GameManager>();
+            }
+            // 싱글톤 오브젝트를 반환
+            return m_instance;
+        }
+    }
+    private static GameManager m_instance;
+
+    public GameObject playerPrefab; // 생성할 플레이어 캐릭터 프리팹
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
+    }
+
+    // 주기적으로 자동 실행되는, 동기화 메서드
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        // 로컬 오브젝트라면 쓰기 부분이 실행됨
+        if (stream.IsWriting)
+        {
+   
+        }
+        else
+        {
+            
+        }
+    }
+
+
+    private void Awake()
+    {
+        // 씬에 싱글톤 오브젝트가 된 다른 GameManager 오브젝트가 있다면
+        if (instance != this)
+        {
+            // 자신을 파괴
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
